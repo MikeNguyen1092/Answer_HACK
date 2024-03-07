ca983f959bf99ba3e416263ea13e729dcbb22172
Ryan McIntyre <rmcintyre598@gmail.com>
2024-03-07 13:28:58 -0500

    messing with the querries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,54 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_QHESTION = gql`
+  query getThoughts {
+    thoughts {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_SINGLE_Question = gql`
+  query getSingleQuestion($questionId: ID!) {
+    question(questionId: $questionId) {
+      _id
+      questionText
+      questionAuthor
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      question {
+        _id
+        questionText
+        questionAuthor
+        createdAt
+      }
+    }
+  }
+`;
