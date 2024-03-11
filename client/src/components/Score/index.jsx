feba386eaa403fdf90bdfd98e919b1beca194294
Robert <robojr181@yahoo.com>
2024-03-11 14:32:45 -0400

    i took out the highscore button on the highscore page and added highScore to each userseed json

--- client/src/components/Score/index.jsx
+++ client/src/components/Score/index.jsx
@@ -1,27 +1,30 @@
 
-const Score=()=> {
-//     function showHighScore() {
+const Score = () => {
+    //     function showHighScore() {
 
-//         const highScore = g();
-//     alert(`High Score: ${highScore}`);
-//     }
+    //         const highScore = g();
+    //     alert(`High Score: ${highScore}`);
+    //     }
 
-//     function getHighScoreFromLocalStorage() {
-//       const savedScore = localStorage.getItem('highScore');
-//       return savedScore || 'No high score yet';
-//     }
+    //     function getHighScoreFromLocalStorage() {
+    //       const savedScore = localStorage.getItem('highScore');
+    //       return savedScore || 'No high score yet';
+    //     }
 
-return(
-<main>
-<div>
-<h1>"High score will go here"</h1></div>
-<div className="Score">
-</div>
-<button onClick="showHighScore()">HighScore
-</button>
-</main>
+    return (
+        <main>
+            <div>
+                <h1>"High score will go here"</h1></div>
+           
+           
+            <script>
 
-);
+            </script>
+
+
+        </main>
+
+    );
 };
 
 export default Score;
